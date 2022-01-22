1ed36aae8b572b203db9ef7bd3bdcddf183b1ff2
Nikita <nikitademedyuk1@gmail.com>
2022-01-22 22:49:07 +0300

    fix bugs

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -10,18 +10,21 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack(alignment: .leading) {
-            VStack {
-                Text("Turtle Rock")
-                    .font(.title)
+            Text("Turtle Rock")
+                .font(.title)
                 .foregroundColor(.black)
-            }
             HStack {
                 Text("Joshua Tree National Park")
                     .font(.subheadline)
+                Spacer()
+                Text("California")
+                    .font(.subheadline)
             }
         }
+        .padding()
     }
 }
+ 
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
